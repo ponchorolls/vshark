d23cf16d4b19e9bf79ef0ead56ff7f35993509c2
ponchorolls <will.vatcher@gmail.com>
2026-02-25 09:11:56 -0800

    "Initial Commit"

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  name = 'vshark'
+  description = "A high-performance network chat-like TUI";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            libpcap
+            wireshark-cli # Provides dumpcap
+            pkg-config
+          ];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.libpcap}/lib:$LD_LIBRARY_PATH
+            echo "Network TUI Dev Environment Loaded"
+          '';
+        };
+      });
+}
